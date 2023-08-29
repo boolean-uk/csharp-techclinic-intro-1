fb98c1ab75beabe10b84d01c9cdb13c0aa8cd9ed
uerbzr <nigel.sibbert@gmail.com>
2023-08-29 16:17:01 +0100

    Add project files.

---
+++ roastreview.console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using roastreview.data;
+
+foreach (var item in StockManager.GetStocks().ToArray())
+{
+    Console.WriteLine($"{item.Id} {item.Name} {item.Rating}");
+}
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ roastreview.data/StockManager.cs
@@ -0,0 +1,23 @@
+﻿using roastreview.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roastreview.data
+{
+    public static class StockManager
+    {
+        public static List<StockItem> StockItems = new List<StockItem>()
+        {
+            new StockItem{ Id=1, Name="Red Eye Coffee", Rating=4},
+            new StockItem{ Id=2, Name="Mocha", Rating=2}
+        };
+
+        public static List<StockItem> GetStocks()
+        {
+            return StockItems;
+        }
+    }
+}
